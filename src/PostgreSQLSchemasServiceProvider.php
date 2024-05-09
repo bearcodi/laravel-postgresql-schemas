4f6e35c41b878e1de8579e903b71e9b1c9a5a3ea
Mark Babic <mark.babic@wheelsmedia.com.au>
2024-05-09 10:19:49 +1000

    Wired up package service provider

---
+++ src/PostgreSQLSchemasServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bearcodi\Laravel\PostgreSQLSchemas;
+
+use Illuminate\Support\ServiceProvider;
+
+class PostgreSQLSchemasServiceProvider extends ServiceProvider
+{
+    public static function boot(): void
+    {
+    }
+}
